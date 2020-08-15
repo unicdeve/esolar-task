8e303fc3806e2fc3b4457560e4d9e8c86befb49e
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-08-15 15:49:37 +0100

    get todos redux actions

--- backend/todo/api/views.py
+++ backend/todo/api/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate
 
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from rest_framework import status, viewsets
 
